7823194c48660097113e68a1d606ec339630599d
Anastasia Potapova <42168776+mindelly@users.noreply.github.com>
2020-06-22 14:58:44 +0300

    auto

--- mpi/merge_sort_c++11.cpp
+++ mpi/merge_sort_c++11.cpp
@@ -6,13 +6,13 @@
 
 using namespace std;
 
-const long N = 100000000;
+const long N = 100;
 
 template<class Iter>
 void merge_sort(Iter first, Iter last)
 {
     if (last - first > 1) {
-        Iter middle = first + (last - first) / 2;
+        auto middle = first + (last - first) / 2;
         merge_sort(first, middle);
         merge_sort(middle, last);
         inplace_merge(first, middle, last);
@@ -24,7 +24,8 @@ int main()
     vector<int> v;
  
     for (int i = 0; i < N; ++i) 
-        v.push_back(i); 
+        v.push_back(i);
+        
     for (size_t i = 0; i < v.size(); ++i) 
         swap(v[i], v[rand() % (v.size() - i) + i]); 
 
@@ -32,7 +33,7 @@ int main()
 
     merge_sort(v.begin(), v.end());
     for(auto n : v) {
-        //std::cout << n << ' ';
+        std::cout << n << ' ';
     }
-    //std::cout << '\n';
+    std::cout << '\n';
 }
